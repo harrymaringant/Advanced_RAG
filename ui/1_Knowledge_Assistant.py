16226c6c00a3f056fb22ce6d96904a22c91dc714
harry.maringant <harry.maringan@banksinarmas.com>
2024-09-05 14:12:31 +0700

    update app

--- ui/1_Knowledge_Assistant.py
+++ ui/1_Knowledge_Assistant.py
@@ -11,9 +11,10 @@ if not os.path.exists(nltk_data_dir):
     os.makedirs(nltk_data_dir, exist_ok=True)
 nltk.data.path.clear()
 nltk.data.path.append(nltk_data_dir)
-nltk.download("stopwords", download_dir=nltk_data_dir)
-nltk.download('punkt', download_dir=nltk_data_dir)
-nltk.download('wordnet', download_dir=nltk_data_dir)
+nltk.download('all', download_dir=nltk_data_dir)
+# nltk.download("stopwords", download_dir=nltk_data_dir)
+# nltk.download('punkt', download_dir=nltk_data_dir)
+# nltk.download('wordnet', download_dir=nltk_data_dir)
 
 import chromadb
 import streamlit as st
