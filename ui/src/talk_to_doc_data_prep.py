876547d9e53d23976056d42d9ce94b3ba95f65e5
harry.maringant <harry.maringan@banksinarmas.com>
2024-09-05 17:51:44 +0700

    update faiss

--- ui/src/talk_to_doc_data_prep.py
+++ ui/src/talk_to_doc_data_prep.py
@@ -1,13 +1,16 @@
 from src.document_reader import DOCUMENTReader
 import streamlit as st
 from llama_index.core import VectorStoreIndex
-from langchain_community.vectorstores import FAISS
-from langchain_google_genai import (
-    GoogleGenerativeAIEmbeddings,
-)
+from llama_index.vector_stores.faiss import FaissVectorStore
+from llama_index.embeddings.gemini import GeminiEmbedding
+from llama_index.core import Settings, StorageContext
+import faiss
 
 gemini_api_key = st.secrets["gemini_api_key"]
-
+text_embedding_model = GeminiEmbedding(api_key=gemini_api_key, model_name="models/text-embedding-004")
+Settings.embed_model = text_embedding_model
+d = 768
+faiss_index = faiss.IndexFlatL2(d)
 
 class Prep:
     """Ingestion class for ingesting documents to vectorstore."""
@@ -30,6 +33,11 @@ class Prep:
         chunks,chunks_faiss = loader.load_document(file_path=file)
         
         # Initialize the vector store
-        faiss_vector_store = FAISS.from_documents(chunks_faiss, embedding=GoogleGenerativeAIEmbeddings(model="models/embedding-001",google_api_key=gemini_api_key))
+        # faiss_vector_store = FaissVectorStore.from_documents(chunks_faiss, emb="models/text-embedding-004",google_api_key=gemini_api_key)
+
+        vector_store = FaissVectorStore(faiss_index=faiss_index)
+        storage_context = StorageContext.from_defaults(vector_store=vector_store)
+        faiss_vector_store = VectorStoreIndex.from_documents(chunks_faiss, storage_context=storage_context, embed_model = text_embedding_model)
+        
         vector_store = VectorStoreIndex.from_documents(chunks)
-        return vector_store,faiss_vector_store
+        return vector_store, faiss_vector_store
